4b58be3cd5eb3fe295327408f356d7ca1a377cb2
IvanBaca <ivan.videogamer@gmail.com>
2021-11-21 13:32:30 -0600

    fixed db connection so it returns readable object

--- db-connection.js
+++ db-connection.js
@@ -1,10 +1,10 @@
 const { createConnection } = require('mysql');
 
 const connection = createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : '',
-  database : 'desktopticketapp'
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'desktopticketapp'
 });
 
 //This is a comment
@@ -17,12 +17,19 @@ const connection = createConnection({
     }
 })*/
 
-function generalQuey(quey, params){
-    connection.query(quey, params, (err, result, fields)=>{
-        if(err){
-            return console.log(err);
-        }
-        return console.log(result);
+function generalQuey(quey, params) {
+    return new Promise(data => {
+        connection.query(quey, params, (err, result, fields) => {
+            if (err) {
+                return console.log(err);
+            }
+            try {
+                data(result);
+            } catch (err) {
+                data({});
+                console.log(err)
+            };
+        });
     });
 }
 
--- src/js/ticket-view.js
+++ src/js/ticket-view.js
@@ -1,5 +1,9 @@
-const connection = require('./../../db-connection');
+const conn = require('./../../db-connection');
 
-console.log(connection.generalQuey('show tables;'))
+async function doStuff(){
+    res1 = await conn.generalQuey('show tables;')
+    console.log(res1)
+    console.log(res1[0].Tables_in_desktopticketapp)
+}
 
-
+doStuff();
